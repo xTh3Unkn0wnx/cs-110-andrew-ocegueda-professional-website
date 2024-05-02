16857d34ea9ee013e33107307ad77581948117ce
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-05-01 23:19:08 -0700

    add changes to repo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cs-110-andrew-ocegueda-professional-website/"
 })
