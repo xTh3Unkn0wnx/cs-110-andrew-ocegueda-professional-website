441a3c848d40096724e01901429331aaa54ac5ff
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-05-02 00:10:22 -0700

    added changes such as a starter for website and adding dependencies such as font awesome icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import photo1 from './assets/Picture1.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <> 
+      <header>
+        <nav>
+          <ul className='nav-list'>
+            <li>Home</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section aria-label='hero section' className='hero-section'>
+          <h1 className='hero-heading'>Andrew Ocegueda</h1>
+          <img src={photo1} alt="Picture of Andrew Ocegueda" className='profile-pic'/>
+          <p className='hero-about-me'>I am Andrew Ocegueda, a Senior Computer Science with Business Applications Student (CSBA) attending University of California, Riverside.</p>
+        </section>
+        <section className='about-me' aria-label='about me'>
+          <h2>About Me</h2>
+          <p>I am a Senior Computer Science with Business Applications Student (CSBA) attending University of California, Riverside. I am a highly motivated individual who is passionate about technology and its applications. I am a quick learner and a team player who is always looking for ways to improve and grow. I am currently seeking a full-time position in software engineering or data science.</p>
+        </section>
+        <section className='education-container' aria-label='education section'>
+          <h2>Education</h2>
+        </section>
+        <section>
+          <h2>Projects</h2>
+        </section>
+        <section>
+          <h2>Achievements</h2>
+        </section>
+      </main>
+      <footer>
+        <a href="https://github.com/xTh3Unkn0wnx">
+         <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.linkedin.com/in/andrew-ocegueda-715206230?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BhesREb0KRy2g6rmzHum7Tg%3D%3D">
+          <FontAwesomeIcon icon={faLinkedin} />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <FontAwesomeIcon icon={faPhone} />
+        <p>Phone: (999) 999-9999</p>
+        <FontAwesomeIcon icon={faEnvelope} />
+        <p>Email: aaaaaa@aaaaa.aaa </p>
+      </footer>
     </>
   )
 }
