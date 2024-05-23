19212d3929e892660f1b62ff9b4b2e012d1ea853
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-05-22 21:30:58 -0700

    Updates

--- assets/Cube-D5AE3qUc.js
+++ assets/Cube-D5AE3qUc.js
@@ -1,4 +1,4 @@
-import{r as ke,g as O0,j as mn}from"./index-Cz-Lza8U.js";/**
+import{r as ke,g as O0,j as mn}from"./index-COL8FvM_.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
