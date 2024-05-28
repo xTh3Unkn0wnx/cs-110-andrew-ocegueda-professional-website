6e9f4a825b8ef2aa70191612df7f3a238f196856
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-05-27 18:54:08 -0700

    Updates

--- assets/Cube-rr4UNJ6F.js
+++ assets/Cube-rr4UNJ6F.js
@@ -1,4 +1,4 @@
-import{r as ke,g as O0,j as mn}from"./index-tVXd9Puv.js";/**
+import{r as ke,g as O0,j as mn}from"./index-C3ZtWgUf.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
