7369db696059d82b1da91d560fa4972ba9260ae0
Andrew Ocegueda <andrewocegueda2018@gmail.com>
2024-11-19 12:02:02 -0800

    removed phone number in footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
   return (
@@ -17,9 +17,6 @@ function Footer() {
         <a href='https://www.linkedin.com/in/andrew-ocegueda-715206230?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BhesREb0KRy2g6rmzHum7Tg%3D%3D'>
           <FontAwesomeIcon icon={faLinkedin} className='icon'/>
         </a>
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faPhone} /> Phone: (951) 314-8381
       </p>
       <p>
         <FontAwesomeIcon icon={faEnvelope} /> Email: andrewocegueda2018@gmail.com
